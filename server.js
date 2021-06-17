d8bd7adbee1e4232fa7d24f8d450341bf1168e72
Cavell Wong <cavellerson@Cavells-MacBook-Air.local>
2021-06-17 09:54:17 -0400

    deploying

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 // const session = require('express-session')
 const client = require('./db')
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 const dotenv = require('dotenv')
 dotenv.config();
 
